c088ff6f9e09deac067b10ee2827b748b6a8bf33
ThomasCluzel <thomas.cluzel@etu.uca.fr>
2019-02-02 13:47:24 +0100

    Optimize the tests by getting rid of the tmp file

--- main.cpp
+++ main.cpp
@@ -1,6 +1,5 @@
 // std C++ includes
 #include <iostream>
-#include <fstream>
 
 // C. Ismay's includes
 #include "libmultivartest/tntjama/tnt.h"
@@ -16,14 +15,17 @@ extern "C" {
 
 using std::cout;
 using std::endl;
-using std::ifstream;
-using std::ofstream;
-using std::flush;
-using std::string;
 using std::exit;
 
+// constants
+const int FLAG_TEST_CORR = 0b001;
+const int FLAG_TEST_MULT = 0b010;
+const int FLAG_TEST_PORT = 0b100;
+const int FLAG_TEST_ALL  = 0b111;
+
 // prototypes of functions
-void runCorrelationTests(const char* filesWithStatus[], const int numberOfStatus);
+void runCorrelationTests(const char* filesWithStatus[], const int numberOfStatus, int flagTestEnable = FLAG_TEST_ALL);
+
 
 // main program that runs tests
 int main(int, char**)
@@ -37,89 +39,68 @@ int main(int, char**)
                                   "mersenne_twister_states/mts000M000000",
                                   "mersenne_twister_states/mts000M000000",
                                   "mersenne_twister_states/mts000M000000",
-                                  "mersenne_twister_states/mts000M000000"};
+                                  "mersenne_twister_states/mts000M000000" };
     const int numberOfStatus = sizeof(statusFiles) / sizeof(statusFiles[0]);
 
     // control experiment
-    runCorrelationTests(statusFiles, numberOfStatus);
+    runCorrelationTests(statusFiles, numberOfStatus, FLAG_TEST_CORR | FLAG_TEST_MULT);
 
     return 0;
 }
 
-/**
- * \fn void runCorrelationTests(string filesWithStatus[])
- * \brief A function to perform the statistical
- *        tests on a bunch of initial status
- * \param filesWithStatus an array of file names that contain status of MT
- */
-void runCorrelationTests(const char* filesWithStatus[], const int numberOfStatus)
+/* A function to perform the statistical */
+/* tests on a bunch of initial status    */
+void runCorrelationTests(const char* filesWithStatus[], const int numberOfStatus, int flagTestEnable)
 {
-    // first open an empty file to store values generated in it
-    const char* tmpfilename = "tmp.txt";
-    ofstream fileContainingNumbers(tmpfilename, ios_base::out | ios_base::trunc);
-    if(!fileContainingNumbers.is_open())
-    {
-        cerr << "Error: could not open file " << tmpfilename << endl;
-        exit(1);
-    }
-
-    // set all the properties required to perform the test
     const int n = 100000/numberOfStatus, p = numberOfStatus; // original values were n=10000, p=10
     TNT::Array2D <long double> mat(n, p);
     double Alpha;
 
-    for(int i=0; i<p; i++) // for each status
+    for(int s=0; s<p; s++) // for each status
     {
-        // restore the status into MT status
-        restoreStatus(filesWithStatus[i]);
-        for(int j=0; j<n; j++)
+        // set MT status to this status
+        restoreStatus(filesWithStatus[s]);
+        for(int r=0; r<n; r++) // for each random number in the sequence
         {
-            // fill the file with a sequence generated from this status
-            fileContainingNumbers << genrand_real1() << "\n";
+            // populate a column of the matrix with the random numbers 
+            mat[r][s] = genrand_real1();
         }
     }
-    fileContainingNumbers << flush;
-    fileContainingNumbers.close();
 
-    // Generate the matrix from the file
-    ifstream fin;
-    fin.open(tmpfilename);
-    if(!fin.is_open()){
-        cout<< "Error opening input file!" << endl;
-        exit(2);
-    }
-    //Input file one row after another
-    for(int i = 0; i < n; i++)
-        for(int j = 0; j < p; j++)
-            fin >> mat[i][j];
-    fin.close();
-    remove(tmpfilename);
+    /*** run all the tests ***/
 
-    // run all the tests
+    //To avoid scientific notation
+    //cout.setf(std::ios::fixed);
 
     //std::cout << "Enter alpha value: " << endl;
     //std::cin >> Alpha;
     Alpha = 0.01;
 
-    mcorr mcorr1 = mcorr(n, p, mat, Alpha);
+    if(flagTestEnable & FLAG_TEST_CORR)
+    {
+        mcorr mcorr1 = mcorr(n, p, mat, Alpha);
 
-    //To avoid scientific notation
-    //cout.setf(std::ios::fixed);
-
-    //Pearson
-    mcorr1.mcorr_pairCorr(0);
-    //Spearman
-    mcorr1.mcorr_pairCorr(1);
-    //Kendall
-    mcorr1.mcorr_pairCorr(2);
-
-    //Testing if correlation matrix = identity
-    mmult mmult1 = mmult(n, p, mat, Alpha);
-    mmult1.mmult_LRT();
-
-    //Portmanteau tests for white noise with lag = p
-    mport mport1 = mport(n, p, mat, Alpha);
-    mport1.mport_centerMat();
-    mport1.mport_portmanteauTests(p);
-    mport1.mport_mahdiMcLeod(p);
+        //Pearson
+        mcorr1.mcorr_pairCorr(0);
+        //Spearman
+        mcorr1.mcorr_pairCorr(1);
+        //Kendall
+        mcorr1.mcorr_pairCorr(2);
+    }
+    
+    if(flagTestEnable & FLAG_TEST_MULT)
+    {
+        //Testing if correlation matrix = identity
+        mmult mmult1 = mmult(n, p, mat, Alpha);
+        mmult1.mmult_LRT();
+    }
+    
+    if(flagTestEnable & FLAG_TEST_PORT)
+    {
+        //Portmanteau tests for white noise with lag = p
+        mport mport1 = mport(n, p, mat, Alpha);
+        mport1.mport_centerMat();
+        mport1.mport_portmanteauTests(p);
+        mport1.mport_mahdiMcLeod(p);
+    }
 }
